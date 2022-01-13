a058d194d467baae5e671d8421a80e22af77fc71
viet pham <phamnhuviet7901@outlook.com>
2022-01-13 20:06:54 +0700

    Add project files.

---
+++ Cau1/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿
+namespace Cau1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtngaysinh = new System.Windows.Forms.DateTimePicker();
+            this.cbgoitinh = new System.Windows.Forms.CheckBox();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.combodonvi = new System.Windows.Forms.ComboBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbnoisinh = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnNEW = new System.Windows.Forms.Button();
+            this.btnDELETE = new System.Windows.Forms.Button();
+            this.btnEDIT = new System.Windows.Forms.Button();
+            this.btnEXIT = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ma";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Don vi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(213, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ho Ten";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(213, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Noi Sinh";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(428, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ngay Sinh";
+            // 
+            // dtngaysinh
+            // 
+            this.dtngaysinh.Location = new System.Drawing.Point(507, 10);
+            this.dtngaysinh.Name = "dtngaysinh";
+            this.dtngaysinh.Size = new System.Drawing.Size(133, 22);
+            this.dtngaysinh.TabIndex = 5;
+            // 
+            // cbgoitinh
+            // 
+            this.cbgoitinh.AutoSize = true;
+            this.cbgoitinh.Location = new System.Drawing.Point(668, 11);
+            this.cbgoitinh.Name = "cbgoitinh";
+            this.cbgoitinh.Size = new System.Drawing.Size(120, 21);
+            this.cbgoitinh.TabIndex = 6;
+            this.cbgoitinh.Text = "Gioi Tinh Nam";
+            this.cbgoitinh.UseVisualStyleBackColor = true;
+            // 
+            // tbId
+            // 
+            this.tbId.Location = new System.Drawing.Point(86, 12);
+            this.tbId.Name = "tbId";
+            this.tbId.Size = new System.Drawing.Size(121, 22);
+            this.tbId.TabIndex = 7;
+            // 
+            // combodonvi
+            // 
+            this.combodonvi.FormattingEnabled = true;
+            this.combodonvi.Location = new System.Drawing.Point(86, 40);
+            this.combodonvi.Name = "combodonvi";
+            this.combodonvi.Size = new System.Drawing.Size(121, 24);
+            this.combodonvi.TabIndex = 8;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(274, 12);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(148, 22);
+            this.tbName.TabIndex = 9;
+            // 
+            // tbnoisinh
+            // 
+            this.tbnoisinh.Location = new System.Drawing.Point(274, 42);
+            this.tbnoisinh.Name = "tbnoisinh";
+            this.tbnoisinh.Size = new System.Drawing.Size(514, 22);
+            this.tbnoisinh.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 339);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // btnNEW
+            // 
+            this.btnNEW.Location = new System.Drawing.Point(470, 415);
+            this.btnNEW.Name = "btnNEW";
+            this.btnNEW.Size = new System.Drawing.Size(75, 23);
+            this.btnNEW.TabIndex = 12;
+            this.btnNEW.Text = "Them";
+            this.btnNEW.UseVisualStyleBackColor = true;
+            // 
+            // btnDELETE
+            // 
+            this.btnDELETE.Location = new System.Drawing.Point(551, 415);
+            this.btnDELETE.Name = "btnDELETE";
+            this.btnDELETE.Size = new System.Drawing.Size(75, 23);
+            this.btnDELETE.TabIndex = 13;
+            this.btnDELETE.Text = "Xoa";
+            this.btnDELETE.UseVisualStyleBackColor = true;
+            // 
+            // btnEDIT
+            // 
+            this.btnEDIT.Location = new System.Drawing.Point(632, 415);
+            this.btnEDIT.Name = "btnEDIT";
+            this.btnEDIT.Size = new System.Drawing.Size(75, 23);
+            this.btnEDIT.TabIndex = 14;
+            this.btnEDIT.Text = "Sua";
+            this.btnEDIT.UseVisualStyleBackColor = true;
+            // 
+            // btnEXIT
+            // 
+            this.btnEXIT.Location = new System.Drawing.Point(713, 415);
+            this.btnEXIT.Name = "btnEXIT";
+            this.btnEXIT.Size = new System.Drawing.Size(75, 23);
+            this.btnEXIT.TabIndex = 15;
+            this.btnEXIT.Text = "Thoat";
+            this.btnEXIT.UseVisualStyleBackColor = true;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "ma";
+            this.Column1.HeaderText = "Ma";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 80;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "hoten";
+            this.Column2.HeaderText = "Ho Ten";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 140;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "ngaysinh";
+            this.Column3.HeaderText = "Ngay Sinh";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 120;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "gioitinh";
+            this.Column4.HeaderText = "Gioi Tinh";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Column4.Width = 120;
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "noisinh";
+            this.Column5.HeaderText = "Noi Sinh";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 130;
+            // 
+            // Column6
+            // 
+            this.Column6.DataPropertyName = "donvi";
+            this.Column6.HeaderText = "Don Vi";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEXIT);
+            this.Controls.Add(this.btnEDIT);
+            this.Controls.Add(this.btnDELETE);
+            this.Controls.Add(this.btnNEW);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tbnoisinh);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.combodonvi);
+            this.Controls.Add(this.tbId);
+            this.Controls.Add(this.cbgoitinh);
+            this.Controls.Add(this.dtngaysinh);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtngaysinh;
+        private System.Windows.Forms.CheckBox cbgoitinh;
+        private System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.ComboBox combodonvi;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbnoisinh;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnNEW;
+        private System.Windows.Forms.Button btnDELETE;
+        private System.Windows.Forms.Button btnEDIT;
+        private System.Windows.Forms.Button btnEXIT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+    }
+}
+
